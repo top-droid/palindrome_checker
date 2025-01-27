bb9022d156db5f98673c03f1ba871fd5f2f6b5c6
Alex Nistor <alexmateiciuc06@gmail.com>
2025-01-27 21:19:32 +0100

    finished project

---
+++ script.js
@@ -0,0 +1,77 @@
+// variable declarations
+const input = document.getElementById("text-input")
+const button = document.getElementById("check-btn")
+const result = document.getElementById("result")
+const regExpPattern = /\W/gi;
+
+
+// function declarations
+
+
+
+
+// toggle hidden class
+const hide = () => {
+    if (result.classList.contains("hidden")) {
+        result.classList.toggle("hidden");
+    } else {
+        // do nothing
+    }
+    
+}
+
+// alert shows up, if input is empty
+const requiredInput = () => {
+    if (input.value.trim().length === 0) {
+        if (!result.classList.contains("hidden")) {
+            result.classList.toggle("hidden");
+        } else {
+            // do nothing
+        }
+        alert("Please input a value")
+        return false;
+    }
+    return true;
+}
+
+// regex removes punctuation, spaces and symbols
+const cleanInput = () => {
+    return input.value.toLowerCase().replace(regExpPattern, "");
+}
+
+// invert cleanInput
+const invertedCleanInput = () => {
+    let invertedInput = "";
+    const integerNumber = cleanInput().length - 1;
+
+    for (let i = integerNumber; i >= 0; i--) {
+        invertedInput += cleanInput()[i];
+    }
+    return invertedInput;
+}
+
+// check if input is a palindrome
+const palindromeCheck = () => {
+    hide();
+    if (cleanInput() === invertedCleanInput()) {
+        result.innerHTML = `${input.value.trim()} is a palindrome`;
+    } else {
+        result.innerHTML = `${input.value.trim()} is not a palindrome`;
+    }
+}
+
+// complete palindrome check function (final function)
+
+const finalFunction = () => {
+    if (requiredInput() === true) {
+        palindromeCheck();
+    } else {
+        // do nothing
+    }
+}
+
+
+// event listeners
+button.addEventListener("click", finalFunction);
+
+
